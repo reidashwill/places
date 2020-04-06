a9940593bcc2cfe28d117b64d1a1f1e1081f9468
Reid Ashwill <reidashwill@gmail.com>
2020-04-06 13:57:48 -0700

    finish backend logic and impletent front end logic to display the .summary method
    co-authored-by Spencer-Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
 
-  var portland = {
-   location = "Oregon",
-   landmark = "the Bluffs",
-   season = "summer" 
-  }
-
+//UI Logic --------
   $(document).ready(function(){
-    event.preventDeafult();
-    
-  })
+    $("#portlandButton").click(function(){
+      $("#result").text(portland.summary());    
+    })
+    $("#seattleButton").click(function(){
+      $("#result").text(seattle.summary());    
+    })
+    $("#newYorkButton").click(function(){
+      $("#result").text(newYork.summary());    
+    })
+  });
 
 
 // Business logic for places
+
 function Place(location, landmark, season) {
- this.location = location;
- this.landmark = landmark;
- this.season = season; 
+  this.location = location;
+  this.landmark = landmark;
+  this.season = season;
 }
+var portland = new Place("Portland, Oregon", "The Bluffs", "summer");
+var seattle = new Place("Seattle, Washington", "The Space Needle", "fall");
+var newYork = new Place("New York, New York", "The Statue of Liberty", "...maybe just don't come")
+  
 
 Place.prototype.summary = function() {
-  return this.landmark + " is located at " + this.location + ", the best time to visit is in the " + this.season
+  return "Feel like taking a vacation? Come on down to " + this.landmark + " in " + this.location + ". The best time to visit is during the " + this.season + "."
 }
